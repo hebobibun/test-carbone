0f681df7bd85a7872201cac7245dde1b4712233d
hebobibun <hebobibun@gmail.com>
2023-05-04 16:45:52 +0700

    generated pdf

--- main.js
+++ main.js
@@ -2,17 +2,42 @@ const fs = require('fs');
 const carbone = require('carbone');
 
 // Data to inject
-var data = {
-  firstname : 'John',
-  lastname : 'Doe'
-};
+var data = [
+  {
+    movieName : 'Matrix',
+    actors    : [{
+      firstname : 'Keanu',
+      lastname  : 'Reeves'
+    },{
+      firstname : 'Laurence',
+      lastname  : 'Fishburne'
+    },{
+      firstname : 'Carrie-Anne',
+      lastname  : 'Moss'
+    }]
+  },
+  {
+    movieName : 'Back To The Future',
+    actors    : [{
+      firstname : 'Michael',
+      lastname  : 'J. Fox'
+    },{
+      firstname : 'Christopher',
+      lastname  : 'Lloyd'
+    }]
+  }
+];
 
 var options = {
   convertTo : 'pdf' //can be docx, txt, ...
 };
 
-carbone.render('./node_modules/carbone/examples/simple.odt', data, options, function(err, result){
+carbone.render('./node_modules/carbone/examples/movies.docx', data, options, function(err, result){
   if (err) return console.log(err);
-  fs.writeFileSync('result.pdf', result);
-  process.exit(); // to kill automatically LibreOffice workers
+  fs.writeFileSync('movies_result.pdf', result);
+});
+
+carbone.render('./node_modules/carbone/examples/flat_table.ods', data, options, function(err, result){
+  if (err) return console.log(err);
+  fs.writeFileSync('flat_table_result.pdf', result);
 });
